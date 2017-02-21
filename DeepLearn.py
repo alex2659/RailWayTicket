e63f6a0045b1ba3f3bf7d3aa84b5b9aeff92c598
vi000246 <vi000246@gmail.com>
2017-02-21 14:17:32 +0800

    修改readme.md

--- DeepLearn.py
+++ DeepLearn.py
@@ -1,37 +1,81 @@
+'''Trains a simple convnet on the MNIST dataset.
+Gets to 99.25% test accuracy after 12 epochs
+(there is still a lot of margin for parameter tuning).
+16 seconds per epoch on a GRID K520 GPU.
+'''
 # encoding: utf-8
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
-from keras.optimizers import SGD
+from __future__ import print_function
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
 from keras.datasets import mnist
-import numpy
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+
+batch_size = 128
+nb_classes = 10
+nb_epoch = 12
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+# number of convolutional filters to use
+nb_filters = 32
+# size of pooling area for max pooling
+pool_size = (2, 2)
+# convolution kernel size
+kernel_size = (3, 3)
+
+# the data, shuffled and split between train and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+if K.image_dim_ordering() == 'th':
+    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
+    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
 
 model = Sequential()
-model.add(Dense(784, 500, init='glorot_uniform')) # 輸入層，28*28=784
-model.add(Activation('tanh')) # 激活函數是tanh
-model.add(Dropout(0.5)) # 採用50%的dropout
 
-model.add(Dense(500, 500, init='glorot_uniform')) # 隱層節點500個
-model.add(Activation('tanh'))
+model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
+                        border_mode='valid',
+                        input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=pool_size))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
 model.add(Dropout(0.5))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
 
-model.add(Dense(500, 10, init='glorot_uniform')) # 輸出結果是10個類別，所以維度是10
-model.add(Activation('softmax')) # 最後一層用softmax
+model.compile(loss='categorical_crossentropy',
+              optimizer='adadelta',
+              metrics=['accuracy'])
 
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True) # 設定學習率（lr）等參數
-model.compile(loss='categorical_crossentropy', optimizer=sgd, class_mode='categorical') # 使用交叉熵作為loss函數
-
-(X_train, y_train), (X_test, y_test) = mnist.load_data() # 使用Keras自帶的mnist工具讀取數據（第一次需要聯網）
-
-X_train = X_train.reshape(X_train.shape[0], X_train.shape[1] * X_train.shape[2]) # 由於mist的輸入數據維度是(num, 28, 28)，這裡需要把後面的維度直接拼起來變成784維
-X_test = X_test.reshape(X_test.shape[0], X_test.shape[1] * X_test.shape[2])
-Y_train = (numpy.arange(10) == y_train[:, None]).astype(int) # 參考上一篇文章，這裡需要把index轉換成一個one hot的矩陣
-Y_test = (numpy.arange(10) == y_test[:, None]).astype(int)
-
-# 開始訓練，這裡參數比較多。batch_size就是batch_size，nb_epoch就是最多迭代的次數， shuffle就是是否把數據隨機打亂之後再進行訓練
-# verbose是屏顯模式，官方這麼說的：verbose: 0 for no logging to stdout, 1 for progress bar logging, 2 for one log line per epoch.
-# 就是說0是不屏顯，1是顯示一個進度條，2是每個epoch都顯示一行數據
-# show_accuracy就是顯示每次迭代後的正確率
-# validation_split就是拿出百分之多少用來做交叉驗證
-model.fit(X_train, Y_train, batch_size=200, nb_epoch=100, shuffle=True, verbose=1, show_accuracy=True, validation_split=0.3)
-print('test set')
-model.evaluate(X_test, Y_test, batch_size=200, show_accuracy=True, verbose=1)
+model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+          verbose=1, validation_data=(X_test, Y_test))
+score = model.evaluate(X_test, Y_test, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
--- README.md
+++ README.md
@@ -10,23 +10,32 @@
 如何安裝
 -----------------------------
 step 1.
-下載安裝OpenVPN 選擇Installer, Windows Vista and later
-https://openvpn.net/index.php/open-source/downloads.html
+    下載安裝OpenVPN 選擇Installer, Windows Vista and later
+    https://openvpn.net/index.php/open-source/downloads.html
 
-並且到OpenVPN的安裝目錄   舉例 E:\OpenVPN\bin
-右鍵點擊openvpn.exe ->內容 ->相容性頁籤 ->將"以系統管理員的身份執行此程式"勾選
+    並且到OpenVPN的安裝目錄   舉例 E:\OpenVPN\bin
+    右鍵點擊openvpn.exe ->內容 ->相容性頁籤 ->將"以系統管理員的身份執行此程式"勾選
 
 Step2.
-安裝Anaconda2  python2.7版本
+    安裝Anaconda2  python2.7版本
 
 Step3.
-安裝以下package
-conda install -c https://conda.binstar.org/menpo opencv
+    安裝以下package
+    conda install -c https://conda.binstar.org/menpo opencv
 
 Step4.
-安裝GCC編譯環境
-指令:conda install mingw libpython
-安裝Keras
-Keras的相關設定請參考這篇
-把backend改成theano
-https://keras-cn.readthedocs.io/en/latest/getting_started/keras_windows/
+    設置keras運行環境
+    Keras的相關設定請參考這篇
+    https://keras-cn.readthedocs.io/en/latest/getting_started/keras_windows/
+
+    記得在環境變數加入這個(GPU加速)
+    C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin
+
+    .theanorc.txt設定檔 最後兩行的註解拿掉
+    device=gpu  <-設為這個才會使用GPU
+
+    P.s. 這份中文教學中的範例代碼已經無法使用 請至下面網址觀看最新範例
+    https://github.com/fchollet/keras/tree/master/examples
+
+
+-------------------------------------------------------
